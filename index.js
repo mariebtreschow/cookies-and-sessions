138e44e90a15aea51836733dfb3d6e9e3fb46266
Marie Treschow <treschow.marie@gmail.com>
2016-11-17 12:04:16 +0100

    simple exercise done

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express'),
-      cookieParser = require('cookie-parser');
+      cookieParser = require('cookie-parser'),
+      pug = require('pug');
 
 
 var app = express();
 
+app.set('view engine', 'pug');
 app.use(express.static('public'));
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-   res.sendFile(__dirname + '/index.html');
+   res.render('index', { visited: req.cookies.visited });
    console.log(req.cookies);
 });
 
